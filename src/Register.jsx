0987c8ca16692b6aa645a251c1224c61258fa0bc
Carina Padurariu <padurariucarina@yahoo.com>
2023-08-09 10:23:22 +0300

    confirm password inserted

--- src/Register.jsx
+++ src/Register.jsx
@@ -4,6 +4,7 @@ export const Register = (props) => {
     const [email, setEmail] = useState('');
     const [pass, setPass] = useState('');
     const [name, setName] = useState('');
+    const [conf_pass, setConf_pass] = useState('');
 
     const handleSubmit = (e) => {
         e.preventDefault();
@@ -14,12 +15,14 @@ export const Register = (props) => {
         <div className="auth-form-container">
             <h2>Register</h2>
         <form className="register-form" onSubmit={handleSubmit}>
-            <label htmlFor="name">Full name</label>
+            <label htmlFor="name">Full Name</label>
             <input value={name} name="name" onChange={(e) => setName(e.target.value)} id="name" placeholder="full Name" />
-            <label htmlFor="email">email</label>
+            <label htmlFor="email">Email</label>
             <input value={email} onChange={(e) => setEmail(e.target.value)}type="email" placeholder="youremail@gmail.com" id="email" name="email" />
-            <label htmlFor="password">password</label>
+            <label htmlFor="password">Password</label>
             <input value={pass} onChange={(e) => setPass(e.target.value)} type="password" placeholder="********" id="password" name="password" />
+            <label htmlFor="password">Confirm Password</label>
+            <input value={conf_pass} onChange={(e) => setConf_pass(e.target.value)} type="confirm password" placeholder="********" id="confirm password" name="confirm password" />
             <button type="submit">Log In</button>
         </form>
         <button className="link-btn" onClick={() => props.onFormSwitch('login')}>Already have an account? Login here.</button>
