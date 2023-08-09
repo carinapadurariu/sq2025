ad43bd0f4621dc474db99f22a9c2b433ea8f1b83
Carina Padurariu <padurariucarina@yahoo.com>
2023-08-10 00:32:29 +0300

    confirm password & errors done

--- src/Register.jsx
+++ src/Register.jsx
@@ -4,12 +4,22 @@ export const Register = (props) => {
     const [email, setEmail] = useState('');
     const [pass, setPass] = useState('');
     const [name, setName] = useState('');
-    const [conf_pass, setConf_pass] = useState('');
+    const [confPass, setConfPass] = useState('');
+    const [error, setError] = useState('');
+
 
     const handleSubmit = (e) => {
         e.preventDefault();
-        console.log(email);
-    }
+    
+        if (pass === confPass) {
+            setError(''); // Curăță mesajul de eroare dacă parolele sunt corecte
+            console.log('Registration successful');
+            // Continuă cu procesul de înregistrare
+        } else {
+            setError('Passwords do not match');
+        }
+    };
+    
 
     return (
         <div className="auth-form-container">
@@ -21,8 +31,9 @@ export const Register = (props) => {
             <input value={email} onChange={(e) => setEmail(e.target.value)}type="email" placeholder="youremail@gmail.com" id="email" name="email" />
             <label htmlFor="password">Password</label>
             <input value={pass} onChange={(e) => setPass(e.target.value)} type="password" placeholder="********" id="password" name="password" />
-            <label htmlFor="password">Confirm Password</label>
-            <input value={conf_pass} onChange={(e) => setConf_pass(e.target.value)} type="confirm password" placeholder="********" id="confirm password" name="confirm password" />
+            <label htmlFor="confirm-password">Confirm Password</label>
+            <input value={confPass} onChange={(e) => setConfPass(e.target.value)} type="password" placeholder="********" id="confirm-password" name="confirm-password" />
+            {error && <p className="error-message">{error}</p>}
             <button type="submit">Log In</button>
         </form>
         <button className="link-btn" onClick={() => props.onFormSwitch('login')}>Already have an account? Login here.</button>
