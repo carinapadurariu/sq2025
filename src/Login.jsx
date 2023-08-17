ebdca36385eb350a50e649f851fee256c48751b9
Ana Tatar <anasabina.tatar@gmail.com>
2023-08-17 08:22:55 +0200

    Added validation for login page

--- src/Login.jsx
+++ src/Login.jsx
@@ -3,20 +3,47 @@ import React, { useState } from "react";
 export const Login = (props) => {
     const [email, setEmail] = useState('');
     const [pass, setPass] = useState('');
+    const [error, setError] = useState('');
+
+    const validateLogin = () => {
+        let isValid = true;
+        const errors = {};
+
+        if (!email) {
+            errors.email = 'Email is required';
+            isValid = false;
+        } else if (!/\S+@\S+\.\S+/.test(email)) {
+            errors.email = 'Invalid email address';
+            isValid = false;
+        }
+
+        if (!pass) {
+            errors.password = 'Password is required';
+            isValid = false;
+        }
+
+        setError(errors);
+        return isValid;
+    }
 
     const handleSubmit = (e) => {
         e.preventDefault();
-        console.log(email);
+        if (validateLogin()) {
+            setError('');
+            console.log('Login successful');
+        }
     }
 
     return (
         <div className="auth-form-container">
             <h2>Login</h2>
             <form className="login-form" onSubmit={handleSubmit}>
-                <label htmlFor="email">email</label>
-                <input value={email} onChange={(e) => setEmail(e.target.value)}type="email" placeholder="youremail@gmail.com" id="email" name="email" />
-                <label htmlFor="password">password</label>
+                <label htmlFor="email">Email</label>
+                <input value={email} onChange={(e) => setEmail(e.target.value)} type="email" placeholder="youremail@gmail.com" id="email" name="email" />
+                {error.email && <p className="error-message">{error.email}</p>}
+                <label htmlFor="password">Password</label>
                 <input value={pass} onChange={(e) => setPass(e.target.value)} type="password" placeholder="********" id="password" name="password" />
+                {error.password && <p className="error-message">{error.password}</p>}
                 <button type="submit">Log In</button>
             </form>
             <button className="link-btn" onClick={() => props.onFormSwitch('register')}>Don't have an account? Register here.</button>
