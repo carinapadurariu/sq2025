0c92347c6423d246f1cf00522c335f295d493b48
Ana Tatar <anasabina.tatar@gmail.com>
2023-09-06 00:14:23 +0300

    Added files

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -5,112 +5,96 @@ import { faFacebook, faGithub, faInstagram } from '@fortawesome/free-brands-svg-
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faEnvelope, faHome,faUser } from '@fortawesome/free-solid-svg-icons';
 import { faMap } from '@fortawesome/free-solid-svg-icons';
+import {useNavigate} from "react-router-dom";
 
 
 export const MyNavBar = (props) => {
 
     const [activeLink, setActiveLink] = useState('home');
     const [scrolled, setScrolled] = useState(false);
-  
+    const navigate = useNavigate();
+
     useEffect(() => {
-      const onScroll = () => {
-        if (window.scrollY > 50) {
-          setScrolled(true);
-        } else {
-          setScrolled(false);
+        const onScroll = () => {
+            if (window.scrollY > 50) {
+                setScrolled(true);
+            } else {
+                setScrolled(false);
+            }
         }
-      }
-  
-      window.addEventListener("scroll", onScroll);
-  
-      return () => window.removeEventListener("scroll", onScroll);
+
+        window.addEventListener("scroll", onScroll);
+
+        return () => window.removeEventListener("scroll", onScroll);
     }, [])
-  
+
     const onUpdateActiveLink = (value) => {
-      setActiveLink(value);
+        setActiveLink(value);
     }
 
-  return (
-    <Navbar expand="lg" className={scrolled ? "scrolled":""}>
-      <Container>
-        <Navbar.Brand href="#home">
-            <img src ={logo} alt="Logo" />
-        </Navbar.Brand>
-        <Navbar.Toggle aria-controls="basic-navbar-nav">
-            <span className="navbar-toggle-icon"></span>
-        </Navbar.Toggle>
-        <Navbar.Collapse id="basic-navbar-nav">
-          <nav className="nav-bar-icons">
-            <Nav.Link exact="true"
-             activeclassname="active" 
-             to="/"
-             onClick = {() => {
-              console.log('gotohome')
-              props.setPageState('home')
-             }
-            }
-             >
-              <FontAwesomeIcon icon={faHome} color="#4d4d4e" />
-            </Nav.Link>
-            <Nav.Link exact="true"
-             activeclassname="active"
-             className="about-link"
-             to="/about"
-             onClick = {() => {
-              console.log('gotoabout')
-              props.setPageState('about')
-             }
-             } >
-              <FontAwesomeIcon icon={faUser} color="#4d4d4e" />
-            </Nav.Link>
-            <Nav.Link exact="true"
-             activeclassname="active" 
-             className="contact-link" 
-             to="/contact"
-             onClick = {() => {
-              console.log('gotocontact')
-              props.setPageState('contact')
-             }
-             } 
-             >
-              <FontAwesomeIcon icon={faEnvelope} color="#4d4d4e" />
-            </Nav.Link>
-            <Nav.Link exact="true" activeclassname="active" className="map-link" to="/map">
-              <FontAwesomeIcon icon={faMap} color="#4d4d4e" />
-            </Nav.Link>
-          </nav>
-          <span className="navbar-text">
+    const handleConnectClick = () => {
+        console.log('connect');
+        navigate('/login');
+    };
+
+    return (
+        <Navbar expand="lg" className={scrolled ? "scrolled":""}>
+            <Container>
+                <Navbar.Brand href="#home">
+                    <img src ={logo} alt="Logo" />
+                </Navbar.Brand>
+                <Navbar.Toggle aria-controls="basic-navbar-nav">
+                    <span className="navbar-toggle-icon"></span>
+                </Navbar.Toggle>
+                <Navbar.Collapse id="basic-navbar-nav">
+                    <nav className="nav-bar-icons">
+                        <Nav.Link exact="true" activeclassname="active" to="/">
+                            <FontAwesomeIcon icon={faHome} color="#4d4d4e" />
+                        </Nav.Link>
+                        <Nav.Link exact="true" activeclassname="active" className="about-link" to="/about">
+                            <FontAwesomeIcon icon={faUser} color="#4d4d4e" />
+                        </Nav.Link>
+                        <Nav.Link exact="true" activeclassname="active" className="contact-link" to="/contact">
+                            <FontAwesomeIcon icon={faEnvelope} color="#4d4d4e" />
+                        </Nav.Link>
+                        <Nav.Link exact="true" activeclassname="active" className="map-link" to="/map">
+                            <FontAwesomeIcon icon={faMap} color="#4d4d4e" />
+                        </Nav.Link>
+                    </nav>
+                    <span className="navbar-text">
             <ul>
               <li>
                 <a target="_blank"
-                 rel="noreferrer" 
-                 href="https://www.facebook.com/padurariu.carina/">
+                   rel="noreferrer"
+                   href="https://www.facebook.com/padurariu.carina/">
 
                   <FontAwesomeIcon icon={faFacebook} color="#4d4d4e" />
                 </a>
               </li>
               <li>
                 <a target="_blank"
-                 rel="noreferrer" 
-                 href="https://github.com/">
+                   rel="noreferrer"
+                   href="https://github.com/">
 
                   <FontAwesomeIcon icon={faGithub} color="#4d4d4e" />
                 </a>
               </li>
               <li>
                 <a target="_blank"
-                 rel="noreferrer" 
-                 href="https://www.instagram.com/">
+                   rel="noreferrer"
+                   href="https://www.instagram.com/">
 
                   <FontAwesomeIcon icon={faInstagram} color="#4d4d4e" />
                 </a>
               </li>
             </ul>
-            <button className="vvd" onClick={() => {console.log('connect'); props.setShowLoginComponent(true)}}><span>Let's Connect</span></button>
+            <button className="vvd" onClick={handleConnectClick}>
+              <span>Let's Connect</span>
+            </button>
           </span>
-        </Navbar.Collapse>
-      </Container>
-    </Navbar>
-  );
+                </Navbar.Collapse>
+            </Container>
+        </Navbar>
+    );
 }
 
